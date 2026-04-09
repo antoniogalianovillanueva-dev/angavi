8dcfa34dcb8ee766df04f61e2bdb08e1701e0949
antoniogalianovillanueva-dev <antonio.galiano.villanueva@gmail.com>
2026-04-09 10:26:11 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import random
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# CORS (para conectar con Vercel)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class InputData(BaseModel):
     cantidad: int
@@ -11,69 +22,45 @@ class InputData(BaseModel):
 
 def frecuencias(seqs, maximo):
     f = [0]*(maximo+1)
-
-    # datos actuales
     for s in seqs:
         for n in s:
             f[n]+=1
-
-    # datos históricos (aprendizaje)
-    try:
-        with open("datos.json","r") as file:
-            for line in file:
-                data = eval(line)
-                for s in data["secuencias"]:
-                    for n in s:
-                        f[n]+=1
-    except:
-        pass
-
     return f
 
-
-
-@app.post("/analizar")
-def analizar(guardar_datos(data)):
-    f = frecuencias(data.secuencias, data.maximo)
-    resultados=[]
-    for i in range(1,data.maximo+1):
-        seq = generar(i,data.cantidad,data.maximo,f)
-        prob = sum(f[n] for n in seq)
-        resultdef generar(inicio, cantidad, maximo, freq):
+def generar(inicio, cantidad, maximo, freq):
     nums = list(range(1,maximo+1))
     nums.remove(inicio)
     res=[inicio]
 
     while len(res)<cantidad:
         nums_sorted = sorted(nums, key=lambda x: -freq[x])
-        
-        # mezcla inteligencia + exploración
-        if random.random() < 0.7:
-            choice = nums_sorted[0]
-        else:
-            choice = random.choice(nums)
-
+        choice = nums_sorted[0]
         res.append(choice)
         nums.remove(choice)
 
-    resultados.append({
-    "inicio":i,
-    "secuencia":seq,
-    "prob":prob,
-    "explicacion": explicar_secuencia(seq, f)
-})
-    
-    reimport json
-
-def guardar_datos(data):
-    with open("datos.json","a") as f:
-        f.write(json.dumps(data.dict())+"\\n")sultados.sort(key=lambda x: x["prob"], reverse=True)
-    return resultados 
+    return res
 
 def explicar_secuencia(seq, freq):
     explicacion = []
     for n in seq:
         explicacion.append(f"Número {n} tiene frecuencia {freq[n]}")
     return " | ".join(explicacion)
-    
-    
+
+@app.post("/analizar")
+def analizar(data: InputData):
+    f = frecuencias(data.secuencias, data.maximo)
+    resultados=[]
+
+    for i in range(1,data.maximo+1):
+        seq = generar(i,data.cantidad,data.maximo,f)
+        prob = sum(f[n] for n in seq)
+
+        resultados.append({
+            "inicio": i,
+            "secuencia": seq,
+            "prob": prob,
+            "explicacion": explicar_secuencia(seq, f)
+        })
+
+    resultados.sort(key=lambda x: x["prob"], reverse=True)
+    return resultados
